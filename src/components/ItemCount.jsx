5c8a781985debb08e664a7281997449442d3f804
ilianaJofre <ilianamicaela@gmail.com>
2021-06-21 19:32:39 -0300

    Cambios en input

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -2,13 +2,19 @@ import { useState } from "react";
 export const ItemCount = (stock) => {
     const [cantidad, setCantidad] = useState (1);
     const [cart, setCart ] = useState([]);
-    console.log(cart);
 
+    console.log(cart);
+    
+    const suma = () => {
+            if (cantidad<stock){
+                setCantidad(cantidad+1)
+            }
+    }
     return (
         <div>
                 <button onClick = { () => { setCantidad(cantidad - 1) }}>-</button>
-                <input type="value" value={cantidad} min="0" max="10" readOnly/>
-                <button onClick = { () => { setCantidad(cantidad + 1) }}>+</button>
+                <input type="number" value={cantidad} readOnly/>
+                <button onClick = {suma}>+</button>
                 <button className='btn btn-primary' onClick={() => { setCart([...cart, {id:1, name:'Test', cantidad: cantidad}]) }}>Agregar al carrito</button>
         </div>
     )
