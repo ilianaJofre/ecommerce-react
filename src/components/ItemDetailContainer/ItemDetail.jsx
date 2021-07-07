6cb2d22cf659cc83e52fcfab21398d18d3dd3fa1
ilianaJofre <ilianamicaela@gmail.com>
2021-07-07 18:50:14 -0300

    Se modifica ItemDetail

--- src/components/ItemDetailContainer/ItemDetail.jsx
+++ src/components/ItemDetailContainer/ItemDetail.jsx
@@ -33,6 +33,11 @@ export const ItemDetail = ({product}, {initial = 1}) => {
                 <p className="card-text">$ {product.price}</p>
             </div>
             <ItemCount resta={resta} suma={suma} onAdd={onAdd} cantidad={cantidad}/>
+            { (cart.length >= 1) ?
+                        <button className='style'><Link className='btn btn-primary' to={'/cart'}>Terminar compra</Link></button>
+                        :
+                        ''
+            }
             <Link  className='btn btn-primary'to={'/'}>Volver</Link>
         </div>
     )
