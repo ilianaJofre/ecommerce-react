5d0d9a962aded6d9c810e89e4fda9c4012c03757
ilianaJofre <ilianamicaela@gmail.com>
2021-07-07 17:25:02 -0300

    Sumo jsx error 404

---
+++ src/components/404.jsx
@@ -0,0 +1,7 @@
+export const notFound = () => {
+    return (
+        <>
+            <img src='/images/404.png' className='w-65 d-flex justifyContent-center mx-auto' alt='Imágen error 404'/>
+        </>
+    )
+}
